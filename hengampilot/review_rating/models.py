c4e02d7395fe3eefe075ceb437592103328149bc
hamidreza <hr.k.amirkabir.iran@gmail.com>
2024-10-27 11:15:17 +0330

    add review model

--- hengampilot/review_rating/models.py
+++ hengampilot/review_rating/models.py
@@ -1,12 +1,45 @@
 from django.db import models
+import uuid
 
 
 class Review(models.Model):
-    pass
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user = models.ForeignKey(
+        "User", on_delete=models.CASCADE, related_name="user_review"
+    )
+    business_id = models.ForeignKey(
+        "Business", on_delete=models.CASCADE, related_name="review_business"
+    )
+    rank = models.IntegerField(
+        choices=[
+            (1, "Very Poor"),
+            (2, "Poor"),
+            (3, "Medium"),
+            (4, "Good"),
+            (5, "Very Good"),
+        ],
+        default=3,
+    )
+    review_text = models.TextField()
+    hidden = models.BooleanField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
 
 class Vote(models.Model):
-    pass
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user = models.ForeignKey("User", on_delete=models.CASCADE, related_name="user_vote")
+    review = models.ForeignKey(
+        "Review", on_delete=models.CASCADE, related_name="review_vote"
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
 
 class ReviewResponse(models.Model):
-    pass
-
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    review = models.ForeignKey(
+        "Review", on_delete=models.CASCADE, related_name="review_response"
+    )
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    
