c9c159a74f61743a5aa0b117dd14297694e968ad
hamidreza <hr.k.amirkabir.iran@gmail.com>
2024-10-27 09:17:25 +0330

    add managers

---
+++ hengampilot/user_management/managers.py
@@ -0,0 +1,31 @@
+from django.contrib.auth.models import BaseUserManager
+
+
+class UserMnagers(BaseUserManager):
+    def create_user(self, email, username, password):
+        if not email:
+            raise ValueError("Users must have an email address")
+        if not username:
+            raise ValueError("Users must have an phone number")
+
+        if not password:
+            raise ValueError("Users must have an password")
+
+        user = self.model(
+            username=username,
+            email=self.normalize_email(email),
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, username, password):
+        """
+        Creates and saves a superuser with the given email, date of
+        birth and password.
+        """
+        user = self.create_user(email, username=username, password=password)
+        user.is_admin = True
+        user.is_superuser = False
+        user.save(using=self._db)
+        return user
