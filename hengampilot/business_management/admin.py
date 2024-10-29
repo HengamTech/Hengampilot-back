78200d45faf6796dd4d645f2b969720506ed7e93
hamidreza <hr.k.amirkabir.iran@gmail.com>
2024-10-29 10:07:52 +0330

    add admin business

--- hengampilot/business_management/admin.py
+++ hengampilot/business_management/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import Business
 
 # Register your models here.
+
+
+class BusinessAdmin(admin.ModelAdmin):
+    list_display = [
+        "business_owner",
+        "status",
+        "business_name",
+        "website_url",
+        "average_rank",
+        "created_at",
+        "updated_at",
+    ]
+    search_fields = ["business_owner", "business_name", "website_url"]
+    list_filter = [
+        "created_at",
+        "updated_at",
+        "status",
+    ]
+
+
+admin.site.register(Business, BusinessAdmin)
