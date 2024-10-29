4822d675480cb1bfbe5e319a7a4237723a4cc1e4
hamidreza <hr.k.amirkabir.iran@gmail.com>
2024-10-29 10:09:06 +0330

    add admin user

--- hengampilot/user_management/admin.py
+++ hengampilot/user_management/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
+from .models import User, Notifications
 
 # Register your models here.
+
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = [
+        "email",
+        "username",
+        "hidden",
+        "is_active",
+        "is_admin",
+        "is_superuser",
+    ]
+    list_filter = [
+        "hidden",
+        "is_active",
+        "is_admin",
+        "is_superuser",
+        "created_at",
+    ]
+    search_fields = ["username", "email"]
+
+
+class NotificationAdmin(admin.ModelAdmin):
+    list_display = ["user_notifications", "is_read"]
+    search_fields = [
+        "user_notifications",
+    ]
+    list_filter = ["created_at", "is_read"]
+
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Notifications, NotificationAdmin)
