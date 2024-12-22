597da3b2016e8b632fe455a3503d0e8ac625fcad
hamidreza <hr.k.amirkabir.iran@gmail.com>
2024-12-22 06:54:01 +0330

    add enpoint get all review from one business

--- hengampilot/business_management/views.py
+++ hengampilot/business_management/views.py
@@ -1,22 +1,30 @@
 from rest_framework import viewsets, status, filters
-from rest_framework.permissions import IsAuthenticated , AllowAny
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from django_filters.rest_framework import DjangoFilterBackend
-from .models import Business, Subscription , Category
+from .models import Business, Subscription, Category
+from drf_spectacular.utils import extend_schema, OpenApiParameter
+
 from .serializers import (
     BusinessSerializer,
     BusinessCreateSerializer,
     BusinessUpdateSerializer,
     SubscriptionSerializer,
-    CategorySerializer # doesn't exists
+    CategorySerializer,  # doesn't exists
 )
+
+from review_rating.models import Review
+from review_rating.serializers import ReviewSerializer
 from .tasks import manage_subscription
+
 
 # ViewSet for managing Business operations (CRUD)
 class BusinessViewSet(viewsets.ModelViewSet):
     queryset = Business.objects.all()  # Get all Business records
-    serializer_class = BusinessSerializer  # Default serializer class for the Business model
+    serializer_class = (
+        BusinessSerializer  # Default serializer class for the Business model
+    )
     permission_classes = [IsAuthenticated]  # Only authenticated users can access
     filter_backends = [
         DjangoFilterBackend,  # Backend for filtering
@@ -24,13 +32,16 @@ class BusinessViewSet(viewsets.ModelViewSet):
         filters.OrderingFilter,  # Backend for ordering results
     ]
     search_fields = ["business_name", "description"]  # Fields to be searchable
-    ordering_fields = ["created_at", "average_rank"]  # Fields by which the results can be ordered
+    ordering_fields = [
+        "created_at",
+        "average_rank",
+    ]  # Fields by which the results can be ordered
 
-    def get_permissions(self): 
-        if self.action in ['list', 'retrieve', 'categories']: 
-            return [AllowAny()] # Allow any user to access these actions 
+    def get_permissions(self):
+        if self.action in ["list", "retrieve", "categories"]:
+            return [AllowAny()]  # Allow any user to access these actions
         return [IsAuthenticated()]
-    
+
     # Dynamically return the appropriate serializer class based on the action
     def get_serializer_class(self):
         # Use the BusinessCreateSerializer for the create action
@@ -46,16 +57,77 @@ class BusinessViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         serializer.save(business_owner=self.request.user, average_rank=0)
 
-    @action(detail=False, methods=["get"], permission_classes=[AllowAny], url_path='categories') 
-    def list_categories(self, request): 
-        categories = Category.objects.all() 
-        serializer = CategorySerializer(categories, many=True) 
+    @action(
+        detail=False,
+        methods=["get"],
+        permission_classes=[AllowAny],
+        url_path="categories",
+    )
+    def list_categories(self, request):
+        categories = Category.objects.all()
+        serializer = CategorySerializer(categories, many=True)
         return Response(serializer.data)
+
+
+    @extend_schema(
+        parameters=[
+            OpenApiParameter(
+                name="name",
+                type=str,
+                location=OpenApiParameter.QUERY,
+                description="Name of the business to retrieve reviews for.",
+                required=False,
+            ),
+            OpenApiParameter(
+                name="id",
+                type=str,
+                location=OpenApiParameter.QUERY,
+                description="ID of the business to retrieve reviews for.",
+                required=False,
+            ),
+        ],
+        responses={
+            200: ReviewSerializer(many=True),
+            400: OpenApiParameter,  # پیام خطا در صورت ورود اشتباه
+            404: OpenApiParameter,  # پیام خطا در صورت عدم وجود شرکت
+        },
+    )
+    @action(
+        detail=False, methods=["get"], permission_classes=[AllowAny], url_path="reviews"
+    )
+    def get_reviews(self, request):
+        business_name = request.query_params.get("name", None)
+        business_id = request.query_params.get("id", None)
+
+        if not business_name and not business_id:
+            return Response(
+                {"error": "Please provide either 'name' or 'id' as a query parameter."},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+        try:
+            if business_name:
+                business = Business.objects.get(business_name=business_name)
+            elif business_id:
+                business = Business.objects.get(id=business_id)
+        except Business.DoesNotExist:
+            return Response(
+                {"error": "Business not found."},
+                status=status.HTTP_404_NOT_FOUND,
+            )
+
+        reviews = Review.objects.filter(business_id=business)
+        serializer = ReviewSerializer(reviews, many=True)
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
 
 # ViewSet for managing Subscription operations (CRUD)
 class SubscriptionVeiw(viewsets.ModelViewSet):
     queryset = Subscription.objects.all()  # Get all Subscription records
-    serializer_class = SubscriptionSerializer  # Default serializer class for the Subscription model
+    serializer_class = (
+        SubscriptionSerializer  # Default serializer class for the Subscription model
+    )
     permission_classes = [IsAuthenticated]  # Only authenticated users can access
 
     # Custom action to manage a subscription (activate, deactivate, change type)
