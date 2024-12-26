560d946e7dcf70048048b84a2f107a92cbcfb2e8
hamidreza <hr.k.amirkabir.iran@gmail.com>
2024-12-26 12:50:52 +0330

    add is active and is super user in serializer

--- hengampilot/user_management/serializers.py
+++ hengampilot/user_management/serializers.py
@@ -2,14 +2,32 @@ from rest_framework import serializers
 from .models import User, Notifications
 from django.contrib.auth.password_validation import validate_password
 
+
 # Serializer for the User model
 class UserSerializer(serializers.ModelSerializer):
     # Add password validation
-    password = serializers.CharField(write_only=True, required=False, allow_blank=True, validators=[validate_password])
+    password = serializers.CharField(
+        write_only=True,
+        required=False,
+        allow_blank=True,
+        validators=[validate_password],
+    )
 
     class Meta:
         model = User
-        fields = ("id", "email", "username", "first_name", "last_name", "password", "is_active", "created_at")
+        fields = (
+            "id",
+            "email",
+            "username",
+            "first_name",
+            "last_name",
+            "password",
+            "is_active",
+            "is_admin",
+            "hidden",
+            "is_superuser",
+            "created_at",
+        )
         extra_kwargs = {"password": {"write_only": True}}
 
     def create(self, validated_data):
@@ -29,14 +47,15 @@ class UserSerializer(serializers.ModelSerializer):
         instance.first_name = validated_data.get("first_name", instance.first_name)
         instance.last_name = validated_data.get("last_name", instance.last_name)
         instance.is_active = validated_data.get("is_active", instance.is_active)
-        
+
         # Check and update password if provided
         password = validated_data.get("password", None)
         if password:
             instance.set_password(password)  # Hash the password
-        
+
         instance.save()
         return instance
+
 
 # Serializer for the Notifications model
 class NotificationSerializer(serializers.ModelSerializer):
