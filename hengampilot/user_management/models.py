000a624a0ef2be40e849c259b9bf5d56c6ceffa0
Ehsan-ML79 <125207813+Ehsan-ML79@users.noreply.github.com>
2024-12-28 19:16:42 +0330

    fixing usr first and last name issue

--- hengampilot/user_management/models.py
+++ hengampilot/user_management/models.py
@@ -1,10 +1,28 @@
 from django.db import models
 import uuid
 from django.db import models
-from django.contrib.auth.models import PermissionsMixin, AbstractBaseUser, AbstractUser
-
+from django.contrib.auth.models import PermissionsMixin, AbstractBaseUser, BaseUserManager
 # Import custom manager for User model
 from .managers import UserMnagers
+
+class UserMnagers(BaseUserManager):
+    def create_user(self, username, email, password=None, **extra_fields):
+        """
+        Creates and saves a User with the given username, email, and password.
+        """
+        if not email:
+            raise ValueError('The Email field must be set')
+        email = self.normalize_email(email)
+        user = self.model(username=username, email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, email, password=None, **extra_fields):
+
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_staff', True)
+        return self.create_user(username, email, password, **extra_fields)
 
 
 # Custom User model that extends AbstractBaseUser and PermissionsMixin
