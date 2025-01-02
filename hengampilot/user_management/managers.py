6da09ac4cbc3b667b86b8d15c985188a0e492196
hamidreza <hr.k.amirkabir.iran@gmail.com>
2025-01-02 11:37:46 +0330

    add image to user

--- hengampilot/user_management/managers.py
+++ hengampilot/user_management/managers.py
@@ -6,34 +6,26 @@ class UserMnagers(BaseUserManager):
 
     # Method to create a regular user
     def create_user(self, email, username, password, is_active):
-        # Check if the email is provided
         if not email:
             raise ValueError("Users must have an email address")
 
-        # Check if the username is provided
         if not username:
             raise ValueError("Users must have a username")
 
-        # Check if the password is provided
         if not password:
             raise ValueError("Users must have a password")
 
-        # Create the user instance and normalize the email
         user = self.model(
             username=username,
             email=self.normalize_email(email),  # Normalize the email to lowercase
         )
 
-        # Set the user's password
         user.set_password(password)
 
-        # Set the user's active status
         user.is_active = is_active
 
-        # Save the user to the database
         user.save(using=self._db)
 
-        # Return the created user instance
         return user
 
     # Method to create a superuser
