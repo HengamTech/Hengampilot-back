3e1f668a84a8a133cb538dac703d61fb39527970
Ehsan-ML79 <125207813+Ehsan-ML79@users.noreply.github.com>
2025-01-11 07:42:59 +0330

    fix docker minor bugs entrypoint and create its doc

--- hengampilot/hengampilot/settings.py
+++ hengampilot/hengampilot/settings.py
@@ -135,8 +135,8 @@ DATABASES = {
         "NAME": "postgres",
         "USER": "username",
         "PASSWORD": "1234",
-        "HOST": "localhost",
-        "PORT": "5433",
+        "HOST": "postgres",
+        "PORT": "5432",
     }
 }
 
